726b6213dc0824cedd5296cab71ff1cdfe3a65c6
Klim <klim_somov@mail.ru>
2022-10-04 09:35:14 +0300

    refactoring

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
-function keyRemover<T>(keys: Array<string>) {
+
+function keyRemover<T>(keys:  Array<string>): (obj: T) => any {
   return (obj: T) => {
-    keys.forEach((e) => {
-      if (Array.isArray(e)) {
-        keyRemover(e);
+    let objClone = JSON.parse(JSON.stringify(obj));
+    for (let key in objClone) {
+      if (typeof objClone[key] === "object") {
+        objClone[key] = removeKeys(objClone[key]);
+      } else {
+        keys.forEach((el) => {
+          delete objClone[el];
+        });
       }
-      delete obj[e as keyof typeof obj];
-    });
-    console.log(obj);
+    }
+    return objClone;
   };
 }
-
 const removeKeys = keyRemover(["a", "b"]);
 const newObj = removeKeys({ a: 1, b: 2, c: 3 }); // { c: 3 }
 const newObj1 = removeKeys({ a: 1, b: 2, c: 3, d: { a: 1, b: 2, c: 3 } }); // { c: 3, d: { c: 3 } }
